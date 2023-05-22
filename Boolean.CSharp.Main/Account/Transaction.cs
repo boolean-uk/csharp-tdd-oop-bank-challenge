65a4968a9641f8876cc888470bfcde826f322f7a
Thanasis Andritsios <thanasis1997abc@gmail.com>
2023-05-22 11:35:38 +0300

    completed with standard... needs impovement though

---
+++ Boolean.CSharp.Main/Account/Transaction.cs
@@ -0,0 +1,56 @@
+﻿using Boolean.CSharp.Main.Enums;
+using Boolean.CSharp.Main.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.Main.Account
+{
+    public class Transaction : ITransaction
+    {
+        private TransactionType _transactionType;
+        private DateTime _transactionTime;
+        private decimal _amount;
+        private decimal _newBalance;
+        
+        public Transaction(DateTime time, TransactionType type, decimal amount, decimal newbalanace) { 
+            _transactionTime = time;
+            _transactionType = type;
+            _amount = amount;
+            _newBalance = newbalanace;
+        
+        
+        }
+
+       /// <summary>
+       /// checking the transaction receipt if the user has an account then returns true anc console logs the receipt
+       /// else it returns false
+       /// </summary>
+       /// <param name="user"></param>
+       /// <param name="account"></param>
+       /// <returns></returns>
+        public bool TransactionsReceipt(User user, Account account) {
+            Console.WriteLine("{0,10} || {1,10} || {2,10} || {3,10} ", "Date", "Transaction Type", "Amount", "Balance");
+            if(account.firstName == user.firstName)
+            {
+
+                foreach (Transaction tran in user.transactions)
+                {
+                    Console.WriteLine("{0,10} || {1,10}       || {2,10} || {3,10} ", $"{tran.transactionTime.ToString("d/M/yyyy")}", $"{tran.TransactionType}", $"{tran.transactionAmount}", $"{tran.newBalance}");
+
+                }
+                return true;
+            }
+            else {  return false; }
+        }
+       
+
+        public TransactionType TransactionType { get { return _transactionType; } set { _transactionType = value; } }
+        public DateTime transactionTime { get { return _transactionTime; } set { _transactionTime = value; } }
+        public decimal transactionAmount { get { return _amount; } set { _amount = value; } }
+        public decimal newBalance { get { return _newBalance; } set { _newBalance = value; } }
+
+    }
+}
--- Boolean.CSharp.Main/Program.cs
+++ Boolean.CSharp.Main/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Boolean.CSharp.Main.Account;
+using Boolean.CSharp.Main.Enums;
+using Boolean.CSharp.Main.Interfaces;
+using Main;
+
 Console.WriteLine("Hello, World!");
+
+//creating a user
+User user3 = new User("Thanasakis", "test", "6999999", 1000m, AccountType.Saving);
+//createing a bank
+Bank bank = new Bank("test");
+
+//creating and account for this user
+bank.CreateAccount(user3);
+
+//creating transaction and making a transaction for a specific user 
+Transaction transaction = new Transaction(DateTime.Today, TransactionType.Credit, 500m,user3.balance);
+bank.MakeTransaction(transaction, user3, bank.BankAccounts.ElementAt(0));
+
+//creating transaction and making a transaction for a specific user 
+
+Transaction transaction2 = new Transaction(DateTime.Now, TransactionType.Debit, 500m, user3.balance);
+bank.MakeTransaction(transaction2, user3, bank.BankAccounts.ElementAt(0));
+
+//printing the receipt for the user 
+transaction.TransactionsReceipt(user3, bank.BankAccounts.ElementAt(0));
+//bank.CreateAccount(user2);
+
+
+
+
+
+
+
+
