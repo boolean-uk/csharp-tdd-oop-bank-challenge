4ef16e939d1dbea1ba43bf24b5d8ad33296c8d3b
joeri luteyn <joeriluteyn@gmail.com>
2023-05-26 12:23:48 +0200

    remade the assignment

--- Boolean.CSharp.Test/ExtensionTests.cs
+++ Boolean.CSharp.Test/ExtensionTests.cs
@@ -1,4 +1,6 @@
 ﻿using Boolean.CSharp.Main;
+using Boolean.CSharp.Main.Enums;
+using Boolean.CSharp.Main.Transactions;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -11,20 +13,58 @@ namespace Boolean.CSharp.Test
     [TestFixture]
     public class ExtensionTests
     {
-        private Extension _extension;
-        public ExtensionTests()
+        
+
+        [Test]
+        public void BalanceTest()
         {
-            _extension = new Extension();
+            Bank bank = new Bank();
+            string accountName = "Test";
+            bank.CreateAccount(accountName, AccountType.Current);
+
+
+            bank.AddTransaction(accountName, AccountType.Current, TransactionType.deposit, 1000);
+            bank.AddTransaction(accountName, AccountType.Current, TransactionType.deposit, 2000);
+            bank.AddTransaction(accountName, AccountType.Current, TransactionType.withdraw, 500);
+
+
+
+            Assert.AreEqual(2500, bank.currentAccounts.FirstOrDefault(i => i._CustomerName == "Test")._balance);
         }
         [Test]
-        private void TestQuestion1()
+        public void BrancheTest()
         {
-
+            Bank bank = new Bank();
+            string accountName = "Test";
+            bank.CreateAccount(accountName, AccountType.Current, BranceType.Athens);
+            Assert.AreEqual(BranceType.Athens, bank.currentAccounts.FirstOrDefault(i => i._CustomerName == accountName)._branceType);
         }
-        [Test]
-        private void TestQuestion2()
-        {
 
+        [TestCase(300, TransactionType.withdraw)]
+
+        public void OverdraftTest(decimal amount, TransactionType transactionType)
+        {
+            Bank bank = new Bank();
+            string name = "joeri";
+            AccountType accountType = AccountType.Current;
+
+            bank.CreateAccount(name, accountType);
+            bank.AddTransaction(name, accountType, transactionType, amount);
+            Assert.AreEqual(1, bank.overdrafts.Count);
+        }
+
+        [TestCase(300, TransactionType.withdraw)]
+
+        public void OverdraftApproveTest(decimal amount, TransactionType transactionType)
+        {
+            Bank bank = new Bank();
+            string name = "joeri";
+            AccountType accountType = AccountType.Current;
+
+            bank.CreateAccount(name, accountType);
+            bank.AddTransaction(name, accountType, transactionType, amount);
+            bank.AcceptOverdraftById(1);
+            Assert.AreEqual(true, bank.overdrafts.FirstOrDefault(i => i._id == 1)._status);
         }
     }
 }
