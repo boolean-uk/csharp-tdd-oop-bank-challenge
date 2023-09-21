807c9017240443f0a53e386763e6bfba5076e56a
ks46 <konastaf46@gmail.com>
2023-09-21 18:07:18 +0300

    CreateSavingsAccountTest passes

--- Boolean.CSharp.Test/AccountTests.cs
+++ Boolean.CSharp.Test/AccountTests.cs
@@ -7,11 +7,20 @@ namespace Boolean.CSharp.Test
     [TestFixture]
     public class AccountTests
     {
-        // I want to create a current account
+        // User Story: I want to create a current account
         [Test]
         public void CreateCurrentAccountTest()
         {
             CurrentAccount current = new CurrentAccount(1);
+
+            Assert.AreEqual(8, current.Number.Length);
+        }
+
+        // User Story: I want to create a savings account
+        [Test]
+        public void CreateSavingsAccountTest()
+        {
+            SavingsAccount current = new SavingsAccount(1);
 
             Assert.AreEqual(8, current.Number.Length);
         }
