052f1696d5c7fe765d4fb04381799a57eb23ab75
Ävadakedavr0 <dylandd4@hotmail.be>
2023-09-21 15:30:03 +0200

    test for printing

--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -13,6 +13,7 @@ namespace Boolean.CSharp.Test
             account.Deposit(100, DateTime.Now);
             Assert.AreEqual(100, account.Balance);
         }
+
         [Test]
         public void DoingWithdrawShouldDecreaseTheBalance()
         {
@@ -20,6 +21,22 @@ namespace Boolean.CSharp.Test
             account.Deposit(200, DateTime.Now);
             account.Withdraw(100, DateTime.Now);
             Assert.AreEqual(100, account.Balance);
+        }
+
+        [Test]
+        public void PrintStateMentShouldReturnFormattedStateMent()
+        {
+            var account = new Account();
+            account.Deposit(1000, new DateTime(1992, 5, 5));
+            account.Deposit(2000, new DateTime(1992, 5, 6));
+            account.Withdraw(999, new DateTime(1992, 5, 7));
+            var statement = account.PrintStatement();
+            var expectedStatement =
+                "date       || credit  || debit  || balance\n" +
+                "07/05/1992 ||         || 999.00 || 2001.00\n" +
+                "06/05/1992 || 2000.00 ||        || 3000.00\n" +
+                "05/05/1992 || 1000.00 ||        || 1000.00";
+            Assert.AreEqual(expectedStatement, statement);
         }
     }
 }
