f88335566e70e259e8cf47248bb61306e4fedfdb
ks46 <konastaf46@gmail.com>
2023-09-21 16:59:15 +0300

    Transaction class and test

---
+++ Boolean.CSharp.Main/Transaction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Boolean.CSharp.Main.Enums;
+
+namespace Boolean.CSharp.Main
+{
+    public class Transaction
+    {
+        private int _id;
+        private DateTime _date;
+        TransactionType _type;
+        private decimal _amount;
+        private decimal _newBalance;
+
+        public Transaction(int id, DateTime date, TransactionType type, decimal amount, decimal prevBalance)
+        {
+            _id = id;
+            _date = date;
+            _type = type;
+            _amount = amount;
+            if (type == TransactionType.Credit)
+            {
+                _newBalance = prevBalance + amount;
+            }
+            else
+            {
+                _newBalance = prevBalance - amount;
+            }
+            
+        }
+
+        public string ToString()
+        {
+            return "";
+        }
+
+    }
+}
---
+++ Boolean.CSharp.Test/TransactionTests.cs
@@ -0,0 +1,24 @@
+﻿using Boolean.CSharp.Main;
+using Boolean.CSharp.Main.Enums;
+
+using NUnit.Framework;
+
+namespace Boolean.CSharp.Test
+{
+    [TestFixture]
+    public class TransactionTests
+    {
+        [Test]
+        public void CreditTransactionToStringTest()
+        {
+            DateTime date = DateTime.Now;
+            decimal amount = 2000m;
+            string expected = $"{date.ToString("dd/MM/yyyy")} || {amount} || \t || {amount}";
+
+            Transaction t = new Transaction(1, date, TransactionType.Credit, amount, 0);
+
+            Assert.AreEqual(expected, t.ToString());
+        }
+
+    }
+}
