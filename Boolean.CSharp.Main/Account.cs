831872a83dcad04cf3d1f4b73bda7e5cc1188eef
Klara Andersson <klara.andersson@se.experis.com>
2024-01-19 10:02:06 +0100

    Implementation done

--- Boolean.CSharp.Main/Account.cs
+++ Boolean.CSharp.Main/Account.cs
@@ -17,19 +17,44 @@ namespace Boolean.CSharp.Main
             transactions = new List<Transaction>();
         }
 
-        public bool DepositFunds(float amount)
+        public bool DepositFunds(float amount, string date)
         {
-            return false;
+            if (amount < 0)
+                return false;
+
+            if (date == "")
+                return false;
+
+            balance += amount;
+            transactions.Add(new Transaction(date, amount, 0.0f, balance));
+            return true;
         }
 
-        public bool WithdrawFunds(float amount)
+        public bool WithdrawFunds(float amount, string date)
         {
-            return false;
+            if (balance < amount)
+                return false;
+
+            if (date == "")
+                return false;
+
+            balance -= amount;
+            transactions.Add(new Transaction(date, 0.0f, amount, balance));
+            return true;
         }
 
         public string GetTransactions()
         {
-            return "";
+            string message = "Date\t|| Credit || Debit || Balance\n";
+
+            for (int i = 0; i < transactions.Count(); i++)
+            {
+                if (i > 0)
+                    message += "\n";
+                message += transactions[i].date + " || " + transactions[i].credit + " || " + transactions[i].debit + " || " + transactions[i].balance;
+            }
+
+            return message;
         }
     }
 }
--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -15,25 +15,27 @@ namespace Boolean.CSharp.Test
             bankAccount = new BankAccount();
             savingsAccount = new SavingsAccount();
 
-            bankAccount.DepositFunds(10000);
-            savingsAccount.DepositFunds(50000);
+            bankAccount.DepositFunds(10000, "23/07/2008");
+            bankAccount.DepositFunds(500, "23/07/2008");
+            bankAccount.WithdrawFunds(100, "24/07/2008");
+            savingsAccount.DepositFunds(50000, "29/02/2016");
         }
 
-        [TestCase(10000, true)]
-        [TestCase(1000, true)]
-        [TestCase(-50, false)]
-        public void DepositMoney(float amount, bool expected)
+        [TestCase(10000, "23/01/2001", true)]
+        [TestCase(1000, "", false)]
+        [TestCase(-50, "26/12/2005", false)]
+        public void DepositMoney(float amount, string date, bool expected)
         {
-            bool result = bankAccount.DepositFunds(amount);
+            bool result = bankAccount.DepositFunds(amount, date);
             Assert.That(expected, Is.EqualTo(result));
         }
 
-        [TestCase(10000, true)]
-        [TestCase(1000000, false)]
-        [TestCase(-50, false)]
-        public void WithdrawMoney(float amount, bool expected)
+        [TestCase(10000, "19/01/2024", true)]
+        [TestCase(1000000, "02/02/1999", false)]
+        [TestCase(-50, "", false)]
+        public void WithdrawMoney(float amount, string date, bool expected)
         {
-            bool result = bankAccount.WithdrawFunds(amount);
+            bool result = bankAccount.WithdrawFunds(amount, date);
             Assert.That(expected, Is.EqualTo(result));
         }
 
