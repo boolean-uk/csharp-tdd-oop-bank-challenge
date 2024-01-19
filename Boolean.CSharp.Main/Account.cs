6b01429e7cb632046334f9c03e52a382bee5182e
Philip Morud <philip.morud@no.experis.com>
2024-01-19 16:01:49 +0100

    Made some changes to domain model and added some of the classes

---
+++ Boolean.CSharp.Main/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.Main
+{
+    public class Account
+    {
+        private string _name;
+        private double _balance;
+    }
+}
---
+++ Boolean.CSharp.Main/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.Main
+{
+    public class Customer
+    {
+
+        private List<Account> _accounts;
+
+        public Customer() { }
+
+        public string CreateSavingsAccount(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
