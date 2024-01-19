37f6f2304eb41d1661dd6af27a101c661b1c614e
Stian Gaustad <stian.gaustad@no.experis.com>
2024-01-19 16:15:08 +0100

    Initial version of tests and domain_model

---
+++ Boolean.CSharp.Main/Documentation/domain_model.md
@@ -0,0 +1,32 @@
+﻿# Core
+
+| Classes | Fields | 
+|-|-|
+| `IAccount` (*abstract*) | `TransactionManager _transactions` `List<User> authorizedUsers` `decimal _balance` |
+| `GeneralAccount` (implementes Account) | |
+| `SavingsAccount` (implementes Account) | |
+| `TransactionManager` | `List<DateTime, desimal> transactions` |
+| `User` (*interface*) | | 
+| `Customer` (*abstract*)(implements User) | `List<IAccount> accounts` `string Name` `DateTime birthDate` `DateTime lastLogin` |
+| `Manager` (implements User) | `List<Customer> customers` `string Name` | 
+
+| Classes | Method | Scenario | Outputs | 
+|-|-|-|-|
+| `Account` | `Deposit(desimal amount)` | Deposit money into the account | True |
+| | | Failed to deposit money into the account | False | 
+| | `Withdraw(desimal amount)` | Withdraw an amount out of the account | decimal | 
+| | `GenerateTransactionRecord(DateTime start, DateTime end)` | Generate a detailed transaction record for the account | Out/Console | 
+| | `GetBalance()` | Retrieve the current balance of the account | decimal | 
+| | `AddUserToAccount(User user)` | Add a new user to the account | true | 
+| | | Failed to add new user to the account | false | 
+| `GeneralAccount` | | | 
+| `SavingsAccount` | | |
+| `IUser` | `GetName()` | Retrieve the name of the user | string |
+| | `GetAccounts()` | Retrieve a list of all accounts associated with the user | List<Account> |
+| | `AddAccountToUser(IAccount account)` | Add the account to the user object | true |
+| | | Failed to add the account to the user | false | 
+| `Customer` | `GetAge()` | Retrieve the customers age | int | 
+| | `OpenNewAccount(string accountType)` | Generate a new account associated with the user | True |
+| | | Failed to generate a new account | False | 
+| `Manager`  | `GetCustomers()` | Retrieve a list of customers associated with this manager | List<Customer> | 
+| `TransactionManager` | `PrintTransactions(DateTime start, DateTime end)` | Print the transaction record between the provided DateTimes | Out/Console | 
--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -1,4 +1,6 @@
 ﻿using Boolean.CSharp.Main;
+using Boolean.CSharp.Main.Accounts;
+using Boolean.CSharp.Main.Users;
 using NUnit.Framework;
 
 namespace Boolean.CSharp.Test
@@ -6,17 +8,61 @@ namespace Boolean.CSharp.Test
     [TestFixture]
     public class CoreTests
     {
-        private Core _core;
-
-        public CoreTests()
+        [Test]
+        public void TestCreateGeneralAccount()
         {
-            _core = new Core();
+            // Arrange
+            IUser user1 = new RegularCustomer();
+            IUser user2 = new Manager();
 
+            // Act
+            bool? res1 = (user1 as RegularCustomer).OpenNewAccount("general");
+            bool? res2 = (user2 as RegularCustomer).OpenNewAccount("general");
+
+            // Assert
+            Assert.That(res1, Is.True);
+            Assert.That(user1.GetAccounts().Count, Is.EqualTo(1));
+            Assert.That(user1.GetAccounts()[0].GetType(), Is.EqualTo(typeof(GeneralAccount)));
+
+            Assert.That(res2, Is.Null);
         }
 
         [Test]
-        public void TestQuestion1()
+        public void TestCreateSavingsAccount()
         {
+            // Arrange
+            IUser user1 = new RegularCustomer();
+            IUser user2 = new Manager();
+
+            // Act
+            bool? res1 = (user1 as RegularCustomer).OpenNewAccount("saving");
+            bool? res2 = (user2 as RegularCustomer).OpenNewAccount("saving");
+
+            // Assert
+            Assert.That(res1, Is.True);
+            Assert.That(user1.GetAccounts().Count, Is.EqualTo(1));
+            Assert.That(user1.GetAccounts()[0].GetType(), Is.EqualTo(typeof(SavingsAccount)));
+
+            Assert.That(res2, Is.Null);
+        }
+
+        [Test]
+        public void TestDepositFunds() 
+        {
+            // Arrange
+            Customer user = new RegularCustomer();
+            user.OpenNewAccount("general");
+            Account account = user.GetAccounts()[0];
+
+            // Act
+            decimal preBalance = account.GetBalance();
+            bool res = account.Deposit(500m);
+            decimal postBalance = account.GetBalance();
+
+            // Assert
+            Assert.That(res, Is.True);
+            Assert.That(preBalance, Is.EqualTo(0m));
+            Assert.That(postBalance, Is.EqualTo(500m));
 
         }
 
