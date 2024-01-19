c85a5e2536af873107cf2dfcdfce849338e3f1cd
Stian Gaustad <stian.gaustad@no.experis.com>
2024-01-19 16:14:52 +0100

    Scaffolding classes and methods

---
+++ Boolean.CSharp.Main/Users/Customer.cs
@@ -0,0 +1,27 @@
+﻿using Boolean.CSharp.Main.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.Main.Users
+{
+    public abstract class Customer : IUser
+    {
+        public List<Account> GetAccounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OpenNewAccount(string accountType) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Boolean.CSharp.Main/Users/IUser.cs
@@ -0,0 +1,16 @@
+﻿using Boolean.CSharp.Main.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.Main.Users
+{
+    public interface IUser
+    {
+        string GetName();
+
+        List<Account> GetAccounts();
+    }
+}
