43f479ec284ef518749b9f5832406df23d7baaa5
Victor <victor.adamson@se.experis.com>
2024-01-19 14:57:57 +0100

    Created test for adding accounts

--- Boolean.CSharp.Main/Core.cs
+++ Boolean.CSharp.Main/Core.cs
@@ -2,13 +2,91 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Transactions;
 
 
 namespace Boolean.CSharp.Main
 {
-    public class Core
+    public enum accountType
     {
+        current,
+        savings
+    }
+    public class Customer
+    {
+        
+        private List<Account> accounts = new List<Account>();
+        public List<Account> Accounts { get { return accounts; } }
+        public Customer()
+        {
 
+        }
+        public Account createAccount(int accountNr, accountType type)
+        {
+            return null;
+        }
+    }
+    public abstract class Account
+    {
+        private string _type;
+        private int _accountNr;
+        private float _balance;
+        public string Type { get { return _type; } }
+        public int AccountNr { get { return _accountNr; } }
+        public float Balance { get { return _balance; } }
+        public Account(string type, int accountNr, float balance)
+        {
+            _type = type;
+            _accountNr = accountNr;
+            _balance = balance;
+        }
+        public void withdraw(int accountNr, float amount)
+        {
 
+        }
+        public void deposit(int accountNr, float amount)
+        {
+
+        }
+    }
+    public class CurrentAccount : Account
+    {
+        public CurrentAccount(string type, int accountNr, float balance) : base(type, accountNr, balance)
+        {
+
+        }
+    }
+    public class SavingsAccount : Account
+    {
+        public SavingsAccount(string type, int accountNr, float balance) : base(type, accountNr, balance)
+        {
+
+        }
+    }
+    public class Transaction
+    {
+        private float _amount;
+        private string _type;
+        private DateTime _timeStamp;
+        public Transaction()
+        {
+
+        }
+        public Transaction createTransaction()
+        {
+            return this;
+        }
+    }
+    public class BankStatement
+    {
+        private List<Transaction> transaction;
+        public BankStatement()
+        {
+
+        }
+        public void printStatement()
+        {
+
+        }
     }
 }
--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -6,18 +6,20 @@ namespace Boolean.CSharp.Test
     [TestFixture]
     public class CoreTests
     {
-        private Core _core;
+        private Customer _customer;
 
         public CoreTests()
         {
-            _core = new Core();
-
+            _customer = new Customer();
         }
 
         [Test]
-        public void TestQuestion1()
+        public void addAccountTest()
         {
-
+            SavingsAccount newSavings = (SavingsAccount)_customer.createAccount(762308, accountType.savings);
+            CurrentAccount newCurrent = (CurrentAccount)_customer.createAccount(556223, accountType.current);
+            Assert.That(_customer.Accounts.Contains(newSavings));
+            Assert.That(_customer.Accounts.Contains(newCurrent));
         }
 
     }
