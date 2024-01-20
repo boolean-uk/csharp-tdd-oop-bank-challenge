585a9c9ca37f36beeac17f4e87d8521a6b1bd557
Sini Suihkonen <sahsuihkonen@gmail.com>
2024-01-20 20:47:50 +0200

    program file cleaned

--- Boolean.CSharp.Main/Program.cs
+++ Boolean.CSharp.Main/Program.cs
@@ -3,17 +3,3 @@ using Boolean.CSharp.Main;
 using System.Reflection.Metadata;
 
 Console.WriteLine("Hello, World!");
-
-Customer customer = new Customer();
-Manager manager = new Manager();
-
-manager.registerCustomer(customer);
-
-GeneralAccount acc = (GeneralAccount)customer.addAccount(AccountType.GENERAL, "ITALY");
-
-acc.MakeTransaction(500.0F, TransactionType.DEPOSIT);
-acc.MakeTransaction(600.0F, TransactionType.WITHDRAW);
-
-bool res= customer.requestOvedraft(acc.ID, 600.0F);
-Console.WriteLine(res);
-Console.WriteLine(acc.getBalance());
