a86b80a6f01f824246b27d46cbf199f85f6674ac
Philip Morud <philip.morud@no.experis.com>
2024-01-22 11:02:37 +0100

    Renamed core tests to customer tests

---
+++ Boolean.CSharp.Test/CustomerTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+
+namespace Boolean.CSharp.Test
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+
+        [SetUp]
+        public void SetUp()
+        {
+            
+
+        }
+
+        [Test]
+        public void CreateSavingAcc()
+        {
+
+        }
+
+        [Test]
+        public void CantCreateDuplicateSavingAcc() 
+        { 
+        
+        }
+
+        [Test]
+        public void CreateCurrentAcc()
+        {
+
+        }
+
+        [Test]
+        public void CantCreateDuplicateCurrentAcc()
+        {
+
+        }
+
+        [Test]
+        public void Deposit() 
+        { 
+        
+        }
+
+        [Test]
+        public void Withdraw()
+        {
+
+        }
+
+        public void CantWithdrawLackingFunds()
+        {
+
+        }
+    }
+}
