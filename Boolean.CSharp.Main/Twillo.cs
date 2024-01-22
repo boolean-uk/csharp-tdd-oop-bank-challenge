1d89bfd5e564dbd219b4d57a5decce23fdb7df60
Sebank <sebank@gmail.com>
2024-01-22 11:18:06 +0100

    sending messages to phones near you

---
+++ Boolean.CSharp.Main/Twillo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.Main
+{
+    public class Twillo : IMessenger
+    {
+        private string accountSid, authToken;
+        public Twillo() 
+        {
+            accountSid = "AC89efc7377e74a2cda9dde441ed88bc73";
+            authToken = "d5c7357eed86c4a8bb92612da02dabca";
+
+            TwilioClient.Init(accountSid, authToken);
+        }
+
+        public void send(string message)
+        {
+            var messageOptions = new CreateMessageOptions(
+                new PhoneNumber("+4791606457"));
+            messageOptions.From = new PhoneNumber("+15022204634");
+            messageOptions.Body = message;
+
+
+            var SMS = MessageResource.Create(messageOptions);
+            Console.WriteLine(SMS.Body);
+        }
+    }
+}
