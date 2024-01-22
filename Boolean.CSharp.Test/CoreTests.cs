e7cf0a7dc9dcc7fb15412853db30c1480eb45652
Kanthee Kosolyuthasarn <Kanthee>
2024-01-22 10:00:39 +0100

    Kanthee Kosolyuthasarn - C#3 - 19.01.2024: Core 2.0

--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -1,4 +1,5 @@
 ﻿using Boolean.CSharp.Main;
+using Boolean.CSharp.Main.Accounts;
 using Boolean.CSharp.Main.Accounts.Constants;
 using Boolean.CSharp.Main.Users;
 using NUnit.Framework;
@@ -29,8 +30,31 @@ namespace Boolean.CSharp.Test
             // There should be 1 user in the bank and the custommer should have 2 accounts.
             Assert.IsTrue(users.Count == 1 && custommer1.getAccAccounts().Count == 2);
 
+        }
+
+        [Test]
+        public void TestMakeCurrentAcc()
+        {
+            BankApplication bankApp = new BankApplication();
+            Custommer custommer1 = new Custommer()
+            {
+                Name = "Kanthee",
+                Branch = Branches.Bergen,
+                Id = 1111
+            };
+
+            bankApp.Add(custommer1);
+            custommer1.makeAccount(AccountType.Current);
+            custommer1.makeAccount(AccountType.Current);
+            int result = custommer1.getAccAccounts().Count(account => account is NormalAcc);
+
+            // The result you return 2 since we have 2 currentAcc.
+            Assert.IsTrue(result == 2);
 
         }
+
+
+
 
     }
 }
