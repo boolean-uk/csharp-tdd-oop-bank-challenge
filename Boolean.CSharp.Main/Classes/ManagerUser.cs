4e69cfd3a2138d41451a71cfb8e75aa8f4637b8c
Benjamin A. E. Darici <benjamin.darici@no.experis.com>
2024-01-22 08:48:12 +0100

    Added class diagram

---
+++ Boolean.CSharp.Main/Classes/ManagerUser.cs
@@ -0,0 +1,13 @@
+﻿using Boolean.CSharp.Main.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.Main.Classes
+{
+    public class ManagerUser : IUser
+    {
+    }
+}
