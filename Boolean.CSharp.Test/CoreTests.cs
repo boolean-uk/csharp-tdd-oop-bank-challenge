53f27889091f9a8df8b9bc92e2933ad3a60dbbe0
Jacob Chen Jensen <jacob.chen.jensen@dk.experis.com>
2024-01-22 10:44:44 +0100

    Print account test

--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -1,22 +1,26 @@
-﻿using Boolean.CSharp.Main;
+﻿using System.Security.Principal;
+using Boolean.CSharp.Main;
 using NUnit.Framework;
 
 namespace Boolean.CSharp.Test
 {
-    [TestFixture]
     public class CoreTests
     {
-        private Core _core;
-
-        public CoreTests()
-        {
-            _core = new Core();
-
-        }
-
+        
         [Test]
-        public void TestQuestion1()
+        public void addMonyToCurrent()
         {
+            Bank bank = new Bank();
+            Customer bob = new Customer("Bob");
+            Account current = new Account("Current");
+            bank.addCustomer(bob);
+            bank.addAccountToCustomer("Cob",current);
+            bank.depositMoney("Bob", "Current", 2000, "14/01/2012");
+
+            List<string> printTest = bank.printStatement("Bob", "Current");
+
+            Assert.IsTrue(printTest[0] == "date       || credit  || debit  || balance");
+            Assert.IsTrue(printTest[1] == "14/01/2012 || 2000.00 ||        || 2000.00");
 
         }
 
