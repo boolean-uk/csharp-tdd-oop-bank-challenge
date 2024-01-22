0c5bfbf222d9aa510623c6a6be1a20f6b6d0ee16
Benjamin A. E. Darici <benjamin.darici@no.experis.com>
2024-01-22 09:35:47 +0100

    Tests for creating current and savings accounts should be ready

--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -1,4 +1,5 @@
 ﻿using Boolean.CSharp.Main;
+using Boolean.CSharp.Main.Classes;
 using NUnit.Framework;
 
 namespace Boolean.CSharp.Test
@@ -6,24 +7,41 @@ namespace Boolean.CSharp.Test
     [TestFixture]
     public class CoreTests
     {
-        private Core _core;
+        private CustomerUser customer;
 
-        public CoreTests()
+        [SetUp]
+        public void SetUp()
         {
-            _core = new Core();
-
+            customer = new CustomerUser();
         }
 
         [Test]
         public void CreatingACurrentAccount()
         {
 
+            //  Arrange - set up test values
+            string test;
+            CurrentAccount testAccount = new CurrentAccount();
+
+            //  Act - use the fucntion we want to test
+            test = customer.CreateAccount(testAccount);
+
+            //  Assert - check the results
+            Assert.That(test, Is.EqualTo("New Account has been made"));
         }
 
         [Test]
         public void CreatingASavingsAccount()
         {
+            //  Arrange - set up test values
+            string test;
+            SavingsAccount testAccount = new SavingsAccount();
 
+            //  Act - use the fucntion we want to test
+            test = customer.CreateAccount(testAccount);
+
+            //  Assert - check the results
+            Assert.That(test, Is.EqualTo("New Account has been made"));
         }
 
         [Test]
