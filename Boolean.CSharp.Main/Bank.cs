7460ee964e1262604ec345213a5bcccca98bd887
llllllll-l <vincent.werelius@gmail.com>
2024-01-23 08:14:23 +0100

    extension class diagram and domain model

--- Boolean.CSharp.Main/Bank.cs
+++ Boolean.CSharp.Main/Bank.cs
@@ -12,14 +12,20 @@ namespace Boolean.CSharp.Main
         private List<OverdraftRequest> _overdraftRequests = new List<OverdraftRequest>();
 
         // Extension
-        public void RequestOverdraft(decimal amount) {
-            OverdraftRequest request = new OverdraftRequest { Amount = amount, IsApproved = false };
-            _overdraftRequests.Add(request);
+        public void RequestOverdraft(IAccount account, decimal amount) {
+            if ((Account)account != null && amount > 0) {
+                OverdraftRequest request = new OverdraftRequest { Amount = amount, IsApproved = false };
+                _overdraftRequests.Add(request);
+            } else {
+                throw new Exception("You must enter an the account you want to file the overdraft amount of!");
+            }
+
+            
         }
 
         // Extension
         public void ApproveOverdraft(int _id) {
-            OverdraftRequest res = _overdraftRequests.FirstOrDefault(req => req.IsApproved == false);
+            OverdraftRequest res = _overdraftRequests.FirstOrDefault(req => req._id);
             if (res != null) {
                 res.IsApproved = true;
             }
