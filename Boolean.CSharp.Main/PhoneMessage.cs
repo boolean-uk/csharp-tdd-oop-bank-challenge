4cfee68fff2186f34194aaa8debce35cc72f99c2
Santhia <santhia.shanmukarasan@gmail.com>
2024-01-25 09:23:43 +0100

    RED Test PhoneMessage

---
+++ Boolean.CSharp.Main/PhoneMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.Main
+{
+    /*This class will have a reference to the associated bank account and a method to 
+     * send the account statement as a message to the customer's phone.*/
+    public class PhoneMessage
+    {
+        public string SendStatement()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Boolean.CSharp.Test/PhoneMessageTest.cs
@@ -0,0 +1,28 @@
+﻿using Boolean.CSharp.Main.Accounts;
+using Boolean.CSharp.Main;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.Test
+{
+    public class PhoneMessageTest
+    {
+        [Test]
+        public void SendStatement_ReturnsNonNullResult()
+        {
+            // Arrange
+            PhoneMessage phoneMessage = new PhoneMessage();
+
+            // Act
+            string result = phoneMessage.SendStatement();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
