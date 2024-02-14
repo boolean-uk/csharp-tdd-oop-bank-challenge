0768827ceb0be59f4f31ae347e74df955d3dcf9b
Sebastian <sebastian.hanssen@outlook.com>
2024-02-14 09:21:01 +0100

    add Account tests

--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -18,5 +18,37 @@ namespace Boolean.CSharp.Test
             Assert.That(bank.customers[0].FirstName, Is.EqualTo("Sebastian"));
         }
 
+        [Test]
+        public void AddCheckingAccount()
+        {
+            //Set Up
+            Bank bank = new Bank(1, BankLocation.Oslo);
+
+            //Execute
+            bank.createCustomer("Sebastian", "Hanssen");
+            bank.customers.FirstOrDefault(x => x.FirstName == "Sebastian" && x.LastName == "Hanssen").CreateChecking("My First Checking Account");
+
+            //verify
+            Assert.That(bank.customers.Count, Is.Not.Null);
+            Assert.That(bank.customers[0].Accounts, Is.Not.Null);
+            Assert.That(bank.customers[0].Accounts[0].Name, Is.EqualTo("My First Checking Account"));
+        }
+
+        [Test]
+        public void AddSavingAccount()
+        {
+            //Set Up
+            Bank bank = new Bank(1, BankLocation.Oslo);
+
+            //Execute
+            bank.createCustomer("Sebastian", "Hanssen");
+            bank.customers.FirstOrDefault(x => x.FirstName == "Sebastian" && x.LastName == "Hanssen").CreateSaving("My First Saving Account");
+
+            //verify
+            Assert.That(bank.customers.Count, Is.Not.Null);
+            Assert.That(bank.customers[0].Accounts, Is.Not.Null);
+            Assert.That(bank.customers[0].Accounts[0].Name, Is.EqualTo("My First Saving Account"));
+        }
+
     }
 }
