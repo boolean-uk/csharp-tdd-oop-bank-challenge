52e7e4882d7f393c0b2465176e014096bbff7cf5
Sebastian <sebastian.hanssen@outlook.com>
2024-02-14 09:39:06 +0100

    create ShowHistory test

--- Boolean.CSharp.Main/AccountModel/Account.cs
+++ Boolean.CSharp.Main/AccountModel/Account.cs
@@ -11,5 +11,22 @@ namespace Boolean.CSharp.Main.AccountModel
         public string Name;
         public double Balance;
         public int FreeWithdrawals;
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if((Balance - amount) < 0)
+            {
+                Console.WriteLine("You don't have enough to withdraw this amount.");
+            } else
+            {
+                Balance -= amount;
+                FreeWithdrawals--;
+            }
+        }
     }
 }
--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -85,5 +85,25 @@ namespace Boolean.CSharp.Test
             Assert.That(bank.customers[0].Accounts[0].Balance, Is.EqualTo(10.00));
             Assert.That(bank.customers[0].Accounts[0].FreeWithdrawals, Is.EqualTo(9999));
         }
+
+        [Test]
+        public void ShowHistory()
+        {
+            //Set Up
+            Bank bank = new Bank(1, BankLocation.Oslo);
+
+            //Execute
+            bank.createCustomer("Sebastian", "Hanssen");
+            bank.customers.FirstOrDefault(x => x.FirstName == "Sebastian" && x.LastName == "Hanssen").CreateChecking("My First Checking Account");
+            bank.customers[0].Accounts[0].Deposit("13/02/2024", 20.00);
+            bank.customers[0].Accounts[0].Withdraw("14/02/2024" ,15.00);
+
+            //verify
+            Assert.That(bank.customers.Count, Is.Not.Null);
+            Assert.That(bank.customers[0].Accounts, Is.Not.Null);
+            Assert.That(bank.customers[0].Accounts[0].Balance, Is.EqualTo(5.00));
+            Assert.That(bank.customers[0].Accounts[0].FreeWithdrawals, Is.EqualTo(9999));
+            Assert.That(bank.customers[0].Accounts[0].ShowHistory(), Is.EqualTo("13/02/2024 | +20.00 | 20.00 , 14/02/2024 | -15.00 | 5.00"));
+        }
     }
 }
