9511e1d2f304b79c406510e0e63498c6f351ac9d
Sebastian <sebastian.hanssen@outlook.com>
2024-02-14 10:03:07 +0100

    pass ShowHistory test

--- Boolean.CSharp.Main/AccountModel/Account.cs
+++ Boolean.CSharp.Main/AccountModel/Account.cs
@@ -11,10 +11,17 @@ namespace Boolean.CSharp.Main.AccountModel
         public string Name;
         public double Balance;
         public int FreeWithdrawals;
+        public List<Transaction> Transactions = new List<Transaction>();
 
         public void Deposit(double amount)
         {
             Balance += amount;
+        }
+
+        public void Deposit(string date, double amount)
+        {
+            Balance += amount;
+            Transactions.Add(new Transaction() { Date = date, AmountChanged = amount, NewBalance = Balance });
         }
 
         public void Withdraw(double amount)
@@ -27,6 +34,53 @@ namespace Boolean.CSharp.Main.AccountModel
                 Balance -= amount;
                 FreeWithdrawals--;
             }
+        }
+
+        public void Withdraw(string date, double amount)
+        {
+            if ((Balance - amount) < 0)
+            {
+                Console.WriteLine("You don't have enough to withdraw this amount.");
+            }
+            else
+            {
+                Balance -= amount;
+                FreeWithdrawals--;
+                Transactions.Add(new Transaction() { Date = date, AmountChanged = -amount, NewBalance = Balance });
+            }
+        }
+
+        public string ShowHistory()
+        {
+            string result = "";
+
+            foreach(Transaction t in Transactions)
+            {
+                if(result == "")
+                {
+                    if(t.AmountChanged > 0)
+                    {
+                        result += $"{t.Date} | +{String.Format("{0:.00}", t.AmountChanged)} | {String.Format("{0:.00}", t.NewBalance)}";
+                    } else
+                    {
+                        result += $"{t.Date} | {String.Format("{0:.00}", t.AmountChanged)} | {String.Format("{0:.00}", t.NewBalance)}";
+                    }
+                    
+                } else
+                {
+                    if (t.AmountChanged > 0)
+                    {
+                        result += $" , {t.Date} | +{String.Format("{0:.00}", t.AmountChanged)} | {String.Format("{0:.00}", t.NewBalance)}";
+                    }
+                    else
+                    {
+                        result += $" , {t.Date} | {String.Format("{0:.00}", t.AmountChanged)} | {String.Format("{0:.00}", t.NewBalance)}";
+                    }
+                }
+                
+            }
+
+            return result;
         }
     }
 }
---
+++ Boolean.CSharp.Main/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.Main
+{
+    public class Transaction
+    {
+        public string Date;
+        public double AmountChanged;
+        public double NewBalance;
+    }
+}
