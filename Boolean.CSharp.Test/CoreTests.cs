ed30737797d2a7860489b3cd55f48a4ca5343f25
Julia <julia.r.lindgren@gmail.com>
2024-08-22 14:40:34 +0200

    Create withdraw tests

--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -54,8 +54,50 @@ namespace Boolean.CSharp.Test
 
             Assert.AreEqual(transactions[0].Amount, 50);
             Assert.AreEqual(transactions[1].Balance, 3050);
+            Assert.AreEqual(transactions[1].Type, TransactionType.Deposit);
             Assert.IsTrue(account.Balance == 3050);
         }
 
+        [Test]
+        public void Withdraw()
+        {
+            Customer customer = new Customer();
+            customer.CreateAccount(AccountType.Current);
+            IAccount account = customer.accounts[0];
+
+            account.Deposit(5000);
+            account.Withdraw(3000);
+
+            var transactions = account.Transactions;
+
+            Assert.AreEqual(transactions[1].Amount, 3000);
+            Assert.AreEqual(transactions[1].Balance, 2000);
+            Assert.AreEqual(transactions[1].Type, TransactionType.Withdraw);
+            Assert.IsTrue(account.Balance == 2000);
+        }
+
+        [Test]
+        public void IllegalWithdraw()
+        {
+            Customer customer = new Customer();
+            customer.CreateAccount(AccountType.Current);
+            IAccount account = customer.accounts[0];
+
+            bool withdraw = account.Withdraw(3000);
+
+            Assert.IsFalse(withdraw);
+        }
+
+        [Test]
+        public void IllegalDeposit()
+        {
+            Customer customer = new Customer();
+            customer.CreateAccount(AccountType.Current);
+            IAccount account = customer.accounts[0];
+
+            bool deposit = account.Deposit(-1000);
+
+            Assert.IsFalse(deposit);
+        }
     }
 }
