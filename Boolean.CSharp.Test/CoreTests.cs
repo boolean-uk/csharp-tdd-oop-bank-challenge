db3ce14f1d4fee81d6bdcfabf25445b678e09d77
Julia <julia.r.lindgren@gmail.com>
2024-08-22 14:01:16 +0200

    Create test for creation of current account

--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -6,18 +6,24 @@ namespace Boolean.CSharp.Test
     [TestFixture]
     public class CoreTests
     {
-        private Core _core;
+
 
         public CoreTests()
         {
-            _core = new Core();
+
 
         }
 
         [Test]
-        public void TestQuestion1()
+        public void CreateAccount()
         {
+            Customer customer = new Customer();
+            customer.CreateAccount(AccountType.Current);
+            customer.CreateAccount(AccountType.Current);
 
+            Assert.AreEqual(customer.accounts[0].Type, AccountType.Current);
+            Assert.AreEqual(customer.accounts[1].Balance, 0m);
+            Assert.IsTrue(customer.accounts.Count() == 2);
         }
 
     }
