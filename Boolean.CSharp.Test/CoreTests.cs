f60e9c539a17983c1bb9185f846239058edb3633
tvaltn <valtniemi@gmail.com>
2024-08-23 10:41:30 +0200

    test1 started

--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -6,17 +6,11 @@ namespace Boolean.CSharp.Test
     [TestFixture]
     public class CoreTests
     {
-        private Core _core;
-
-        public CoreTests()
-        {
-            _core = new Core();
-
-        }
 
         [Test]
-        public void TestQuestion1()
+        public void Test1CreateACurrentAccount()
         {
+            CurrentAccount currentAccount = new CurrentAccount();
 
         }
 
