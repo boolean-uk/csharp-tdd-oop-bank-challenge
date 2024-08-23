6ee15bf148d4750ca3875f4132aa6c194d6f7b93
tvaltn <valtniemi@gmail.com>
2024-08-23 11:12:56 +0200

    test6 pass

--- Boolean.CSharp.Main/BankAccount.cs
+++ Boolean.CSharp.Main/BankAccount.cs
@@ -9,10 +9,12 @@ namespace Boolean.CSharp.Main
     public abstract class BankAccount
     {
         private decimal _balance = 0;
+        List<BankStatement> _bankStatements = new List<BankStatement>();
 
         public bool Deposit(decimal amount)
         {
             _balance += amount;
+            _bankStatements.Add(new BankStatement(DateTime.Now, amount, "Deposit", _balance));
             return true;
         }
 
@@ -20,11 +22,12 @@ namespace Boolean.CSharp.Main
         {
             if (amount > _balance) return false;
             _balance -= amount;
+            _bankStatements.Add(new BankStatement(DateTime.Now, amount, "Withdraw", _balance));
             return true;
         }
 
         public decimal Balance { get { return _balance; } }
 
-        public List<BankStatement> BankStatements { get; set; }
+        public List<BankStatement> BankStatements { get { return _bankStatements; } }
     }
 }
