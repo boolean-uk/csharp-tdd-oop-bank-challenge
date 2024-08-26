b5e90f17558d89b204a4a708d0c3c20c18f753da
bjorgastrid <bjorgkrist@hotmail.com>
2024-08-26 11:48:27 +0200

    core domain model and class diagram

---
+++ Boolean.CSharp.Main/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.Main
+{
+    public abstract class Account
+    {
+        List<Transaction> TransactionHistory;
+
+        public string Depocit(decimal amount) { return ""; }
+
+        public string Withdraw(decimal amount) { return ""; }
+
+        public string BankStatement() { return ""; }
+    }
+}
---
+++ Boolean.CSharp.Main/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.Main
+{
+    public class Customer
+    {
+        List<Account> Accounts { get; }
+
+        public void CreateSavingsAccount() { }
+
+        public void CreateCurrentAccount() { }
+
+        public string DepositToAccount() { return "";  }
+
+        public string WithdrawFromAccount() { return ""; }
+    }
+}
