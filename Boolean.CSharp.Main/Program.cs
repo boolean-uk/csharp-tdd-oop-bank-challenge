4b12e135df9cc2ecb3088a67165412062045d462
Silje Gjesdal Jacobsen <siljegja@gmail.com>
2024-08-28 11:17:11 +0200

    finished

--- Boolean.CSharp.Main/Program.cs
+++ Boolean.CSharp.Main/Program.cs
@@ -7,10 +7,7 @@ account.Deposit(1000, TransactionType.Deposit);
 account.Deposit(2000, TransactionType.Deposit);
 account.Withdraw(500, TransactionType.Withdraw);
 
-foreach (var i in account.Transactions)
-{
-    Console.WriteLine($"{i.TransactionType} {i.Amount} - New Balance: {i.Balance} ");
-}
+
 
 Console.WriteLine();
 
