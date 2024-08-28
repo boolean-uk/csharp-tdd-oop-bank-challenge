cf7efbb8e86ff73268fd6db07fff94f346984158
agrmet <agron-metaj@hotmail.com>
2024-08-28 13:13:00 +0200

    RED: Deposit, withdraw, overdraft

--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -9,47 +9,89 @@ namespace Boolean.CSharp.Test
         [Test]
         public void Deposit()
         {
+            ExpensesAccount account = new (1, "0768631232");
 
+            bool result = account.Deposit(1000f);
+
+            Assert.That(result, Is.True);
         }
         [Test]
         public void DepositNothing()
         {
+            ExpensesAccount account = new(1, "0768631232");
 
+            bool result = account.Deposit(0f);
+
+            Assert.That(result, Is.False);
         }
         [Test]
         public void DepositNegative()
         {
+            ExpensesAccount account = new(1, "0768631232");
 
+            bool result = account.Deposit(-1000f);
+
+            Assert.That(result, Is.False);
         }
         [Test]
         public void Withdraw()
         {
+            ExpensesAccount account = new(1, "0768631232");
+
+            account.Deposit(1000f);
+            bool result = account.Withdraw(1000f);
+
+            Assert.That(result, Is.True);
 
         }
         [Test]
         public void WithdrawNegative()
         {
+            ExpensesAccount account = new(1, "0768631232");
 
+            account.Deposit(1000f);
+            bool result = account.Withdraw(-1000f);
+
+            Assert.That(result, Is.False);
         }
         [Test]
         public void WithdrawMoreThanBalance()
         {
+            ExpensesAccount account = new(1, "0768631232");
 
+            account.Deposit(1000f);
+            bool result = account.Withdraw(2000f);
+
+            Assert.That(result, Is.False);
         }
         [Test]
         public void Overdraft()
         {
+            ExpensesAccount account = new(1, "0768631232");
 
+            bool result = account.Overdraft(500f);
+
+            Assert.That(result, Is.True);
         }
         [Test]
         public void OverdraftBeyondLimit()
         {
+            ExpensesAccount account = new(1, "0768631232");
 
+            bool result = account.Overdraft(1500f);
+
+            Assert.That(result, Is.False);
         }
         [Test]
         public void OverdraftPositiveBalance()
         {
+            ExpensesAccount account = new(1, "0768631232");
 
+            account.Deposit(2000f);
+            bool result = account.Overdraft(1500f);
+
+            // You cant overdraft an amount <= balance
+            Assert.That(result, Is.False);
         }
         [Test]
         public void GenerateValidStatement()
