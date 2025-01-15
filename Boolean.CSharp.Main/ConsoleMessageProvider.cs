b35534565340d28482c195d57c785306300c6e40
magnus195 <22998711+magnus195@users.noreply.github.com>
2025-01-15 10:49:06 +0100

    Passing message tests now.

--- Boolean.CSharp.Main/ConsoleMessageProvider.cs
+++ Boolean.CSharp.Main/ConsoleMessageProvider.cs
@@ -4,6 +4,17 @@ public class ConsoleMessageProvider : IMessageProvider
 {
     public void Send(string message)
     {
-        throw new NotImplementedException();
+        /*
+         THese colors are broken
+         Console.BackgroundColor = ConsoleColor.Yellow;
+         Console.ForegroundColor = ConsoleColor.Black;
+        */
+        
+        const string yellowBackground = "\u001b[43m";
+        const string blackForeground = "\u001b[30m";
+        const string reset = "\u001b[0m";
+
+        Console.WriteLine("\n\nMessage incoming:\n\n");
+        Console.WriteLine($"{yellowBackground}{blackForeground}{message}{reset}");
     }
 }
