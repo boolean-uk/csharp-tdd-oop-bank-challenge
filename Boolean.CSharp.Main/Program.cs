43717a7aa651ba38cd0f3c20cfa86149520322b6
Kristian Sylte <kristianbsylte@gmail.com>
2025-01-15 13:33:45 +0100

    Added small program to show log

--- Boolean.CSharp.Main/Program.cs
+++ Boolean.CSharp.Main/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Boolean.CSharp.Main;
+
+var account = new Account { Name = "main account" };
+account.Deposit(140.0m);
+account.Deposit(39.99m);
+account.Withdraw(15.15m);
+Console.WriteLine(account.GetTransactionLog());
