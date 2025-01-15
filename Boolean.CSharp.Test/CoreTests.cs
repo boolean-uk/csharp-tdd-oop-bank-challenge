1dca329f3ffaf305c721673410340969b8defd12
Espen Solhaug <espen.solhaug@no.experis.com>
2025-01-15 09:06:35 +0100

    Domain-model & Core tests

--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -1,4 +1,5 @@
-﻿using Boolean.CSharp.Main;
+﻿using System.Security.Principal;
+using Boolean.CSharp.Main;
 using NUnit.Framework;
 
 namespace Boolean.CSharp.Test
@@ -6,19 +7,53 @@ namespace Boolean.CSharp.Test
     [TestFixture]
     public class CoreTests
     {
-        private Core _core;
-
-        public CoreTests()
+        public Bank bank { get; set; }
+        [SetUp]
+        public void SetUp()
         {
-            _core = new Core();
-
+            bank = new Bank();
         }
 
         [Test]
-        public void TestQuestion1()
+        public void CreateNewAccountOnExistingCustomerTest()
         {
-
+            Customer customer = new Customer(1, "John", bank);
+            bank.CustomerList.Add(customer);
+            Account account = bank.CreateAccount(customer, "C");
+            Assert.That(account, Is.EqualTo(customer.Account[0]));
+            Assert.Equals(account.Type, CurrentAccount.Type);
         }
+
+        [Test]
+        public void createNewAccountOnNonExistingCustomerTest()
+        {
+            Customer customer = new Customer(1, "John", bank);
+            Account account = bank.CreateAccount(customer, "S");
+            Assert.That(account, Is.EqualTo(customer.Account[0]));
+            Assert.Equals(account.Type, SavingsAccount.Type);
+        }
+
+        [Test]
+        public void DepositAmountToSavingsAccountTest()
+        {
+            Customer customer = new Customer(1, "John", bank);
+            bank.CustomerList.Add(customer);
+            Account customerAccount = bank.CreateAccount(customer, "S");
+            double depositBalance = customer.Deposit(customerAccount, 100);
+            Assert.Equals(customerAccount.Balance, depositBalance);
+        }
+
+        [Test]
+        public void WithDrawFromSavingsAccount()
+        {
+            Customer customer = new Customer(1, "John", bank);
+            bank.CustomerList.Add(customer);
+            Account customerAccount = bank.CreateAccount(customer, "S");
+            customer.deposit(customerAccount, 100);
+            double withdrawBalance = customer.Withdraw(customerAccount, 90);
+            Assert.Equals(customerAccount.Balance, withdrawBalance);
+        }
+
 
     }
 }
