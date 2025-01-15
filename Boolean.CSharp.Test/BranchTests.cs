f5167eb655111278986fb3d01849848491a323db
Miadog7Extra <petter.karstensen@gmail.com>
2025-01-15 11:10:34 +0100

    First test and class

---
+++ Boolean.CSharp.Test/BranchTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Boolean.CSharp.Main;
+
+namespace Boolean.CSharp.Test
+{
+    [TestFixture]
+    public class BranchTests
+    {
+        [Test]
+        public void branchTest()
+        {
+            Branch branch = new Branch("Gåsebu Branch");
+
+            Assert.AreEqual("Gåsebu Branch", branch.Name());
+        }
+
+    }
+}
